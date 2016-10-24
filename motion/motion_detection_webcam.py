87b3d53144d7b8985c21d85c6bc2b2e461660c41
Markus Bajones <markus.bajones@gmail.com>
2016-10-24 16:25:47 +0200

    Add debug option. Restructure code.
    Taking parts from justbill2020 PR #311.

--- motion/motion_detection_webcam.py
+++ motion/motion_detection_webcam.py
@@ -1,25 +1,53 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 import cv2
 import subprocess
+from datetime import datetime as dt
 from time import sleep
 
-MIN_AREA = 500
+MIN_AREA = 500  # how much pixel area needs to change to qualify as motion
 SLEEP_TIME = 100
+MIN_TIME_DELTA = 5  # seconds
+
+isDebug = True  # print debug information
+last_on_timestamp = None  # time when we last turned on the display
+
+
+def debugging(msg):
+    global isDebug
+    if isDebug:
+        print msg
+
+
+def monitor_on():
+    global last_on_timestamp
+    last_on_timestamp = dt.now()
+    debugging("Turn display on")
+    if not isDebug:
+        subprocess.Popen('tvservice -p', shell=True)
+        subprocess.Popen('fbset -depth 8 && fbset -depth 16 && xrefresh', shell=True)
+
+
+def get_monitor_state():
+    process = subprocess.Popen(['tvservice', '-s'], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    debugging(out)
+
 
 def detect_motion():
+    global last_on_timestamp
     cam = cv2.VideoCapture(0)
     last_img = None
-    
-    while True:
-        outcome = False
-        ret, img = cam.read()
-        
-        # quit if we do not get data from webcam
-        if not ret:
-            print("Unable to get data from webcam")
 
-        img_small = cv2.resize(img,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+    while True:
+        ret, img = cam.read()
+
+        # Quit if we do not get data from webcam
+        if not ret:
+            debugging("Unable to get data from webcam. Exiting...")
+            exit(1)
+
+        img_small = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
         gray_img = cv2.cvtColor(img_small, cv2.COLOR_BGR2GRAY)
         blur_img = cv2.GaussianBlur(gray_img, (21, 21), 0)
 
@@ -27,7 +55,7 @@ def detect_motion():
             last_img = blur_img
             continue
 
-        # compute difference between current and last image
+        # Compute difference between current and last image
         img_delta = cv2.absdiff(last_img, blur_img)
         thresh = cv2.threshold(img_delta, 25, 255, cv2.THRESH_BINARY)[1]
 
@@ -35,21 +63,25 @@ def detect_motion():
         (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for cnt in cnts:
-            # ignore small contours
-            print(cv2.contourArea(cnt))
+            # Ignore small contours
             if cv2.contourArea(cnt) < MIN_AREA:
                 continue
             else:
-                print("change detected")
-                subprocess.call(['/opt/vc/bin/tvservice', '-p'])
-                subprocess.call(['fbset -depth 8'], shell=True)
-                subprocess.call(['fbset -depth 16'], shell=True)
-                subprocess.call(['xrefresh'], shell=True)
+                debugging("Motion detected")
+                if not last_on_timestamp:
+                    debugging("First time motion was detected")
+                    monitor_on()
+                else:
+                    time_diff = (dt.now() - last_on_timestamp).total_seconds()
+                    if time_diff > MIN_TIME_DELTA:
+                        debugging("Minimal time delta exceeded")
+                        monitor_on()
 
         last_img = blur_img
-        # give it a little pause (in milliseconds)
-        sleep(SLEEP_TIME/1000.0)
-        
+
+        # Give the motion detection a little pause (in milliseconds)
+        sleep(SLEEP_TIME / 1000.0)
+
 
 if __name__ == "__main__":
     detect_motion()
